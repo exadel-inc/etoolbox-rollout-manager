f9bad76b5767f46afa8070febd0c89568d8167de
Ann Barmina <abarmina@exadel.com>
2025-08-11 19:45:03 +0200

    [AEMaaCS] Refactor rollout status message handling

--- ui.apps/src/main/content/jcr_root/apps/etoolbox-rollout-manager/clientlibs/rollout-manager-ui/js/console-ui.actions.js
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-rollout-manager/clientlibs/rollout-manager-ui/js/console-ui.actions.js
@@ -90,18 +90,23 @@
         }
     }
 
-    const ROLLOUT_COMMAND = '/content/etoolbox/rollout-manager/servlet/rollout';
     const PROCESSING_ERROR_MSG = Granite.I18n.get('Rollout failed');
-    const PROCESSING_ERROR_FAILED_PATHS_MSG = Granite.I18n.get('Rollout failed for the following paths:');
-    const SUCCESS_MSG = Granite.I18n.get('Completed');
-    const SUCCESS_REPLICATION_MSG = Granite.I18n.get('Rollout is completed. Publishing is in progress.');
+    const PROCESSING_ERROR_400_MSG = Granite.I18n.get('Rollout failed for the following paths:');
+    const PROCESSING_SERVER_MSG = Granite.I18n.get('This operation will take some time to complete. Check the target pages later to confirm the result.');
 
-    function getProcessingErrorMsg(xhr) {
+    function getRolloutStatusMessage(xhr) {
         if (xhr.status === 400 && xhr.responseJSON && xhr.responseJSON.failedTargets) {
-            return `${PROCESSING_ERROR_FAILED_PATHS_MSG}<br/><br/>${xhr.responseJSON.failedTargets.join('<br/>')}`;
+            return `${PROCESSING_ERROR_400_MSG}<br/><br/>${xhr.responseJSON.failedTargets.join('<br/>')}`;
+        }
+        if ([503, 504, 522, 524].includes(xhr.status)) {
+            return PROCESSING_SERVER_MSG;
         }
         return PROCESSING_ERROR_MSG;
     }
+
+    const ROLLOUT_COMMAND = '/content/etoolbox/rollout-manager/servlet/rollout';
+    const SUCCESS_REPLICATION_MSG = Granite.I18n.get('Rollout is completed. Publishing is in progress.');
+    const SUCCESS_MSG = Granite.I18n.get('Completed');
 
     /**
      * Builds a request to the servlet for rolling out items based on data collected in the Rollout dialog.
@@ -125,7 +130,7 @@
                 });
                 data.shouldActivate ? logger.log(SUCCESS_REPLICATION_MSG, false) : logger.log(SUCCESS_MSG, false);
             } catch (xhr) {
-                logger.log(getProcessingErrorMsg(xhr), false);
+                logger.log(getRolloutStatusMessage(xhr), false);
             }
         };
     }
