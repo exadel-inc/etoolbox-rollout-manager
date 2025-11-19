b4b9de1a659075bb6174699bd366a8e3b8c9362f
Stepan Miakchilo <smiakchilo@exadel.com>
2025-11-19 23:04:46 +0300

    [ERM-64] Create the task history console

---
+++ ui.apps/src/main/content/jcr_root/apps/etoolbox-rollout-manager/clientlibs/rollout-manager-history/js/script.js
@@ -0,0 +1,16 @@
+(function (document, $) {
+    'use strict';
+
+    function onRefreshClick() {
+        $('#history').adaptTo('foundation-collection').reload();
+    }
+
+    function onShowExtraClick() {
+        $(this).closest('td').addClass('shows-extra');
+    }
+
+    $(document).off('erm-history')
+        .on('click.erm-history', '#refresh', onRefreshClick)
+        .on('click.erm-history', '.show-extra', onShowExtraClick);
+
+})(document, Granite.$);
